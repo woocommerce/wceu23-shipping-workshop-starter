c51cc97bb4fc8a2e49cd5b323340727630aa9dd9
Thomas Roberts <thomas.roberts@automattic.com>
2023-05-12 14:43:21 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+	templatesPath: __dirname,
+	defaultValues: {
+		slug: 'shipping-workshop',
+		title: 'WordCamp EU Shipping Workshop',
+		dashicon: 'palmtree',
+		version: '1.0.0',
+		npmDependencies: [
+			"@wordpress/icons@^9.22.0",
+			"lodash@^4.17.21",
+		],
+		npmDevDependencies: [
+			"@woocommerce/dependency-extraction-webpack-plugin@^2.2.0",
+			"@woocommerce/eslint-plugin@^2.2.0",
+			"@wordpress/prettier-config@^2.14.0",
+			"@wordpress/scripts@^26.2.0"
+		],
+	},
+};
