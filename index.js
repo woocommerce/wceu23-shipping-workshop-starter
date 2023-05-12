8e7c89778372a63664851c2b3a08e4fefd2ccb08
Thomas Roberts <thomas.roberts@automattic.com>
2023-05-12 15:32:17 +0100

    Add wp-env script and dependency

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ module.exports = {
 			"@wordpress/icons@^9.22.0",
 			"lodash@^4.17.21",
 		],
+		customScripts: {
+			"env": "wp-env"
+		},
 		npmDevDependencies: [
 			"@woocommerce/dependency-extraction-webpack-plugin@^2.2.0",
 			"@woocommerce/eslint-plugin@^2.2.0",
 			"@wordpress/prettier-config@^2.14.0",
-			"@wordpress/scripts@^26.2.0"
+			"@wordpress/scripts@^26.2.0",
+			"@wordpress/env@^7.0.0"
 		],
 	},
 };
