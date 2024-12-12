39857678b0962d5ba987899a8f154e7f3449093d
Thomas Roberts <5656702+opr@users.noreply.github.com>
2024-12-12 14:30:10 +0000

    Update index.js
    Co-authored-by: Niels Lange <niels.lange@a8c.com>

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = {
 		npmDevDependencies: [
 			"@woocommerce/dependency-extraction-webpack-plugin@^3.0.1",
 			"@woocommerce/eslint-plugin@^2.3.0",
-			"@wordpress/prettier-config@^4.13.0",
+			"@wordpress/prettier-config@^4.14.0",
 			"@wordpress/scripts@^30.6.0",
 			"@wordpress/env@^10.13.0"
 		],
